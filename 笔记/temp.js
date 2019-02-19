3df7fd0b066716ac823c175c96ae00dd77a2078a
yaowen2011 <tanghaihua1212@163.com>
2019-02-19 22:01:26 +0800

    fea for interview

---
+++ 笔记/temp.js
@@ -0,0 +1,49 @@
+//
+function sidEffecting(ary) {
+  ary[0] = ary[2]
+}
+function bar(a, b, c) {
+  c = 10;
+  sidEffecting(arguments);
+  return a + b +c;
+}
+bar(1,1,1)// 10 + 1 + 10 
+
+// 这个很重要
+var arr = new Array(3);
+arr[0] = 2;
+arr.map(function(element) {
+  return '1'
+})
+console.log(arr.join(','))
+
+// 和参数同名的内部变量
+function foo(a) {
+  var a;
+  return a;
+}
+function bar(a) {
+  var a = 'bye'
+  return a;
+}
+[foo('hello'), bar('hello')]
+
+// 闭包以及自调用
+for (var i = 0; i< 100; i++) {
+  (function (i) {
+    setTimeout(function() {
+      console.log(i)
+    }, 1000*i)
+  })(i)
+}
+// b成了全局变量
+(function() {
+  var a = b = 2;
+})();
+console.log(b)
+
+// 原型链
+function f() {}
+var a = f.__proto__, b = Object.getPrototypeOf(f)
+console.log(a === b) // false
+// console.log(b)
